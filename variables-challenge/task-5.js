fc5042b46a7d470ffbc45b54775f82ebddc6f8c9
20Suleman <20sulemans@gmail.com>
2026-02-22 14:11:51 +0530

    task-5

---
+++ variables-challenge/task-5.js
@@ -0,0 +1,14 @@
+    const textArea = document.querySelector("#textArea");
+    const searchInput = document.querySelector("#searchWord");
+    const button = document.querySelector("#searchBtn");
+
+    button.addEventListener("click", function () {
+        const text = textArea.value.toLowerCase();
+        const word = searchInput.value.toLowerCase();
+
+        if (word && text.includes(word)) {
+            alert("Word exists");
+        } else {
+            alert("Word not present");
+        }
+           });
